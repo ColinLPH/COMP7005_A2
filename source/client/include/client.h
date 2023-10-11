e8e74c4cc6e2d10377c80452ac0e435d23d4653e
Colin Lam <colin.lph.lam@gmail.com>
2023-10-10 18:19:04 -0700

    adjusted client: - wordexp - connects over network socket
    untested

--- source/client/include/client.h
+++ source/client/include/client.h
@@ -1,17 +1,30 @@
 #ifndef A1_CLIENT_H
 #define A1_CLIENT_H
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/errno.h>
 #include <sys/stat.h>
 #include <sys/fcntl.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <wordexp.h>
 
-#define DOMAIN_PATH "/tmp/dompath"
 #define SLEEP_TIMER 3
+#define DEFAULT_ARGS_NUM 4
+#define FILE_INDEX 3
+#define PORT_INDEX 2
+#define IP_INDEX 1
+
+struct client_opts
+{
+    char *dest_ip;
+    in_port_t dest_port;
+    int domain;
+};
 
 struct file_info {
     int filefd;
@@ -29,15 +42,18 @@ struct file_info_list {
 void print_list(struct file_info_list *head);
 
 int run_client(int argc, char *argv[]);
-int creat_socket(void);
-int parse_args(int argc, char *argv[], struct file_info_list *head);
+int creat_socket(struct client_opts *opts);
+int parse_args(int argc, char *argv[], struct file_info_list *head, struct client_opts *opts);
+int get_address_domain(const char *addr);
+in_port_t parse_in_port_t(const char *str);
+int get_files(int argc, char *argv[], struct file_info_list *head);
 int check_files(struct file_info_list *head);
 void get_file_infos(struct file_info_list *head);
 uint8_t *sanitize_file_name(char *file_path);
-int do_send_all(struct file_info_list *head);
-int connect_to_server(int sockfd);
+int do_send_all(struct file_info_list *head, struct client_opts *opts);
+int connect_to_server(int sockfd, struct client_opts *opts);
 void send_file_content(int src_fd, int dest_fd, size_t count);
-void setup_socket_address(struct sockaddr_un *addr);
+void setup_socket_address(struct sockaddr_un *addr, struct client_opts *opts);
 void free_file_info_list(struct file_info_list *head);
 
 #endif //A1_CLIENT_H
--- source/client/src/client.c
+++ source/client/src/client.c
@@ -1,8 +1,10 @@
+#include <arpa/inet.h>
 #include "client.h"
 
 int run_client(int argc, char *argv[])
 {
     struct file_info_list *head;
+    struct client_opts opts;
     int check;
 
     head = malloc(sizeof (struct file_info_list)); // DECISION: throw this into a function?
@@ -10,10 +12,11 @@ int run_client(int argc, char *argv[])
     head->next = NULL;
     head->file_path = NULL;
 
-    check = parse_args(argc, argv, head); //parse args
+    memset(&opts, 0, sizeof(struct client_opts));
+
+    check = parse_args(argc, argv, head, &opts); //parse args
     if(check == -1)
     {
-        printf("At least one file must be provided\n");
         free_file_info_list(head);
         return check;
     }
@@ -22,13 +25,12 @@ int run_client(int argc, char *argv[])
     check = check_files(head); //check if all files can be opened
     if (check != 0)
     {
-        printf("Error: the file \"%s\" could not be opened\n", argv[check]);
         free_file_info_list(head);
         return -1;
     }
     get_file_infos(head); //compile all the data
     print_list(head);
-    check = do_send_all(head);
+    check = do_send_all(head, &opts);
     if (check != 0)
     {
         printf("Failed to send files\n");
@@ -42,36 +44,135 @@ int run_client(int argc, char *argv[])
     return 0;
 }
 
-int parse_args(int argc, char *argv[], struct file_info_list *head)
+int parse_args(int argc, char *argv[], struct file_info_list *head, struct client_opts *opts)
 {
     //tokenize argv
     //get all the file names
     //store them all into head and all the nexts
-    if (argc == 1)
+    if (argc < DEFAULT_ARGS_NUM)
     {
-        //no files were passed
+        printf("Usage: ./client <ip 4 or 6 address to connect to> <port> <files>\n");
         return -1;
     }
 
-    head->file_path = strdup(argv[1]);
-    if (argc > 2)
-    {
-        printf("more file paths detected\n");
-        struct file_info_list *current_node;
-        int num;
+    opts->dest_ip = strdup(argv[IP_INDEX]);
+    opts->domain = get_address_domain(opts->dest_ip);
+    opts->dest_port = parse_in_port_t(argv[PORT_INDEX]);
 
-        current_node = head;
-        num = 2;
-        while(num != argc)
+    if (get_files(argc, argv, head) == -1)
+    {
+        printf("wordexp failed\n");
+        return -1;
+    }
+
+    return 0;
+
+}
+
+int get_address_domain(const char *address)
+{
+    int domain;
+
+    if(strstr(address, ":"))
+    {
+        domain = AF_INET6;
+    }
+    else if (strstr(address, "."))
+    {
+        domain = AF_INET;
+    }
+    else
+    {
+        fprintf(stderr, "Invalid IP address \"%s\"\n", address);
+        exit(EXIT_FAILURE);
+    }
+
+    return domain;
+
+}
+
+in_port_t parse_in_port_t(const char *str)
+{
+    char *endptr;
+    uintmax_t parsed_value;
+
+    parsed_value = strtoumax(str, &endptr, 10);
+
+    if (errno != 0)
+    {
+        perror("Error parsing in_port_t");
+        exit(EXIT_FAILURE);
+    }
+
+    // Check if there are any non-numeric characters in the input string
+    if (*endptr != '\0')
+    {
+        printf("Invalid characters in input.\n");
+    }
+
+    // Check if the parsed value is within the valid range for in_port_t
+    if (parsed_value > UINT16_MAX)
+    {
+        printf("in_port_t value out of range.\n");
+    }
+
+    return (in_port_t)parsed_value;
+}
+
+int get_files(int argc, char *argv[], struct file_info_list *head)
+{
+    struct file_info_list *current_node;
+    wordexp_t p;
+    int ret;
+
+    current_node = head;
+
+    //check if first file arg is expandable
+    ret = wordexp(argv[FILE_INDEX], &p, 0);
+    if(ret == -1)
+    {
+        return -1;
+    }
+
+    //copy first "expanded" file arg into head->file_path
+    current_node->file_path = strdup(p.we_wordv[0]);
+    if(p.we_wordc > 1) //if first arg was expandable, iterate thru expanded first arg and store into linked list
+    {
+        for(size_t i = 1; i < p.we_wordc; ++i)
         {
             current_node->next = malloc(sizeof(struct file_info_list));
             current_node = current_node->next;
-            current_node->file_path = strdup(argv[num]);
+            current_node->file_path = strdup(p.we_wordv[i]);
             current_node->file_info_struct = NULL;
             current_node->next = NULL;
-            ++num;
         }
     }
+    wordfree(&p);
+
+    if(argc > DEFAULT_ARGS_NUM)
+    {
+        //repeat for rest of file args
+        for(int i = FILE_INDEX+1; i < argc; ++i)
+        {
+            ret = wordexp(argv[i], &p, 0);
+            if(ret == 0)
+            {
+                for (size_t j = 0; j < p.we_wordc; ++j) {
+                    current_node->next = malloc(sizeof(struct file_info_list));
+                    current_node = current_node->next;
+                    current_node->file_path = strdup(p.we_wordv[j]);
+                    current_node->file_info_struct = NULL;
+                    current_node->next = NULL;
+                }
+                wordfree(&p);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+
     return 0;
 }
 
@@ -81,18 +182,16 @@ int check_files(struct file_info_list *head)
     //or maybe return the file so that it can be displayed
     struct file_info_list *file_to_check;
     int fd;
-    int num;
 
-    num = 0;
     file_to_check = head;
     while(file_to_check != NULL)
     {
         fd = open(file_to_check->file_path, O_RDONLY);
         printf("opening file: %s\n", file_to_check->file_path);
-        ++num;
         if(fd == -1)
         {
-            return num;
+            printf("failed to open file: %s\n", file_to_check->file_path);
+            return -1;
         }
         file_to_check->file_info_struct = malloc(sizeof(struct file_info));
         file_to_check->file_info_struct->filefd = fd;
@@ -137,20 +236,20 @@ uint8_t *sanitize_file_name(char *file_path)
     return sanitized_name;
 }
 
-int do_send_all(struct file_info_list *head)
+int do_send_all(struct file_info_list *head, struct client_opts *opts)
 {
     int sockfd;
     int ret;
     struct file_info_list *node_to_send;
     node_to_send = head;
-    sockfd = creat_socket(); // socket
+    sockfd = creat_socket(opts); // socket
     if (sockfd == -1)
     {
         printf("Failed to create socket, closing program\n");
         return -1;
     }
 
-    ret = connect_to_server(sockfd); // connect
+    ret = connect_to_server(sockfd, opts); // connect
     while(node_to_send != NULL)
     {
         if(ret == -1)
@@ -169,21 +268,21 @@ int do_send_all(struct file_info_list *head)
     return 0;
 }
 
-int creat_socket(void)
+int creat_socket(struct client_opts *opts)
 {
     int sockfd;
 
-    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    sockfd = socket(opts->domain, SOCK_STREAM, 0);
 
     return sockfd;
 }
 
-int connect_to_server(int sockfd)
+int connect_to_server(int sockfd, struct client_opts *opts)
 {
     struct sockaddr_un addr;
     int ret;
 
-    setup_socket_address(&addr);
+    setup_socket_address(&addr, opts);
 
     ret = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
     if(ret == -1)
@@ -197,16 +296,41 @@ int connect_to_server(int sockfd)
         }
     }
 
-    printf("Connected to %s\n", DOMAIN_PATH);
+    printf("Connected to socket: %s:%u\n", opts->dest_ip, opts->dest_port);
     return 0;
 }
 
-void setup_socket_address(struct sockaddr_un *addr)
+void setup_socket_address(struct sockaddr_un *addr, struct client_opts *opts)
 {
     memset(addr, 0, sizeof(*addr));
-    addr->sun_family = AF_UNIX;
-    strncpy(addr->sun_path, DOMAIN_PATH, sizeof(addr->sun_path) - 1);
-    addr->sun_path[sizeof(addr->sun_path) - 1] = '\0';
+
+    if(inet_pton(opts->domain, opts->dest_ip, &addr) != 1)
+    {
+        perror("Invalid IP address");
+        exit(EXIT_FAILURE);
+    }
+
+    if(opts->domain == AF_INET)
+    {
+        struct sockaddr_in *ipv4_addr;
+
+        ipv4_addr = (struct sockaddr_in *)&addr;
+        ipv4_addr->sin_family = AF_INET;
+        ipv4_addr->sin_port = htons(opts->dest_port);
+    }
+    else if(opts->domain == AF_INET6)
+    {
+        struct sockaddr_in6 *ipv6_addr;
+
+        ipv6_addr = (struct sockaddr_in6 *)&addr;
+        ipv6_addr->sin6_family = AF_INET6;
+        ipv6_addr->sin6_port = htons(opts->dest_port);
+    }
+    else
+    {
+        fprintf(stderr, "Invalid domain: %d\n", opts->domain);
+        exit(EXIT_FAILURE);
+    }
 }
 
 void send_file_content(int src_fd, int dest_fd, size_t count)
@@ -259,6 +383,7 @@ void free_file_info_list(struct file_info_list *head)
             {
                 free(node_to_delete->file_info_struct->file_name);
             }
+            close(node_to_delete->file_info_struct->filefd);
             free(node_to_delete->file_info_struct);
         }
         if(node_to_delete != NULL)
