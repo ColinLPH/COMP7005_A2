7b1d3509d0da31de91a02387d567b5a6ad362e54
Colin Lam <colin.lph.lam@gmail.com>
2023-10-13 15:48:49 -0700

    first try at implementing select()

--- source/server/include/server.h
+++ source/server/include/server.h
@@ -2,6 +2,8 @@
 #define A1_SERVER_H
 
 #include <ctype.h>
+#include <inttypes.h>
+#include <netinet/in.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,13 +14,22 @@
 #include <sys/stat.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <wordexp.h>
+
 
 #define DOMAIN_PATH "/tmp/dompath"
 #define SERVER_BACKLOG 5
+#define SERVER_ARGS 4
+#define DIR_INDEX 3
+#define PORT_INDEX 2
+#define IP_INDEX 1
 
 struct server_options
 {
+    char *host_ip;
+    int domain;
     char *dir_path;
+    in_port_t host_port;
 };
 
 struct file_info {
@@ -30,16 +41,20 @@ struct file_info {
 
 int run_server(int argc, char *argv[]);
 int parse_args(struct server_options *opts, int argc, char *argv[]);
+int set_domain(struct server_options *opts);
+in_port_t parse_in_port_t(const char *str);
 int is_valid_char(char c);
 void sanitize_path(char *path);
 int make_dir(char *dir_path);
-int creat_socket(void);
-int do_bind(int bindfd);
+int creat_socket(int domain, int type, int options);
+int do_bind(int bindfd, struct server_options *opts);
 int do_listen(int bindfd, int backlog);
-void handle_connection(int acceptfd, struct sockaddr_storage *client_addr, struct server_options *opts);
+//void handle_connection(int acceptfd, struct sockaddr_storage *client_addr, struct server_options *opts);
+void handle_data_in(int client_fd, struct server_options *opts, fd_set *readfds, int *client_sockets, size_t i);
 int create_file(struct server_options *opts, struct file_info *file);
 void copy_paste(int src_fd, int dest_fd, int64_t count);
 char *generate_file_name(char *dir_path, char *file_name);
+void socket_close(int fd);
 static void setup_signal_handler(void);
 
 #endif //A1_SERVER_H
--- source/server/src/server.c
+++ source/server/src/server.c
@@ -1,5 +1,11 @@
+#include <arpa/inet.h>
 #include "server.h"
 
+/* things to change
+    - take in new args
+    - change to tcp
+    - implement i/o multiplexing
+*/
 static volatile sig_atomic_t exit_flag = 0;
 
 int run_server(int argc, char *argv[])
@@ -8,8 +14,12 @@ int run_server(int argc, char *argv[])
     //sock, bind, listen, accept
     struct server_options opts;
     int bindfd;
-
-    unlink(DOMAIN_PATH);
+    int *client_sockets = NULL;
+    size_t max_clients = 0;
+    int max_fd, activity, new_socket, sd;
+    int addrlen;
+    struct sockaddr_un address;
+    fd_set readfds;
 
     memset(&opts, 0, sizeof(struct server_options));
 
@@ -24,20 +34,20 @@ int run_server(int argc, char *argv[])
         return -1;
     }
 
-    bindfd = creat_socket();
+    bindfd = creat_socket(opts.domain, SOCK_STREAM, 0);
     if(bindfd == -1)
     {
         free(opts.dir_path);
         return -1;
     }
 
-    if(do_bind(bindfd) == -1)
+    if(do_bind(bindfd, &opts) == -1)
     {
         free(opts.dir_path);
         return -1;
     }
 
-    if(do_listen(bindfd, SERVER_BACKLOG) == -1)
+    if(do_listen(bindfd, SOMAXCONN) == -1)
     {
         free(opts.dir_path);
         return -1;
@@ -46,52 +56,174 @@ int run_server(int argc, char *argv[])
     setup_signal_handler();
     while(!exit_flag)
     {
-        int acceptfd;
-        struct sockaddr_storage client_addr;
-        socklen_t client_addr_len;
+        // Clear the socket set
+        FD_ZERO(&readfds);
 
-        client_addr_len = sizeof(client_addr);
-        acceptfd = accept(bindfd, (struct sockaddr *)&client_addr, &client_addr_len);
-        if(acceptfd == -1)
+        // Add the server socket to the set
+        FD_SET((unsigned int)bindfd, &readfds);
+        max_fd = bindfd;
+
+        // Add the client sockets to the set
+        for(size_t i = 0; i < max_clients; i++)
         {
-            if(exit_flag)
+            sd = client_sockets[i];
+
+            if(sd > 0)
             {
-                break;
+                FD_SET((unsigned int)sd, &readfds);
             }
-            perror("Error accepting incoming connection!\n");
-            continue;
+
+            if(sd > max_fd)
+            {
+                max_fd = sd;
+            }
         }
-        printf("Going to handle connection...\n");
-        handle_connection(acceptfd, &client_addr, &opts);
-        close(acceptfd);
+
+        // Use select to monitor sockets for read readiness
+        activity = select(max_fd + 1, &readfds, NULL, NULL, NULL);
+
+        if(activity < 0)
+        {
+            perror("Select error");
+            return -1;
+        }
+
+        // Handle new client connections
+        if(FD_ISSET((unsigned int)bindfd, &readfds))
+        {
+            if((new_socket = accept(bindfd, (struct sockaddr *) &address, (socklen_t * ) & addrlen)) == -1)
+            {
+                perror("Accept error");
+                return -1;
+            }
+
+            printf("New connection established\n");
+
+            // Increase the size of the client_sockets array
+            max_clients++;
+            client_sockets = (int *)realloc(client_sockets, sizeof(int) * max_clients);
+            client_sockets[max_clients - 1] = new_socket;
+        }
+
+        // Handle incoming data from existing clients
+        for(size_t i = 0; i < max_clients; i++)
+        {
+            sd = client_sockets[i];
+
+            if(FD_ISSET((unsigned int)sd, &readfds))
+            {
+                //handle data in
+                handle_data_in(sd, &opts, &readfds, client_sockets, i);
+            }
+        }
+
+    }
+
+    // Cleanup and close all client sockets
+    for(size_t i = 0; i < max_clients; i++)
+    {
+        sd = client_sockets[i];
+        if(sd > 0)
+        {
+            socket_close(sd);
+        }
     }
 
     free(opts.dir_path);
-    close(bindfd);
-    unlink(DOMAIN_PATH);
-
+    // Free the client_sockets array
+    free(client_sockets);
+    socket_close(bindfd);
+    printf("Server exited successfully.\n");
     return 0;
 }
 
 int parse_args(struct server_options *opts, int argc, char *argv[])
 {
-    if(argc == 1)
+    if(argc != SERVER_ARGS)
     {
-        perror("Must provide a directory to store files in\n");
+        printf("Usage: ./server/build <ip4 or ip6 addr to bind to> <port> <dir to store in>\n");
         return -1;
     }
-    if(argc > 2)
+
+    opts->host_ip = strdup(argv[IP_INDEX]);
+    if(set_domain(opts) == -1)
     {
-        perror("Must provide only one directory to store files in\n");
+        //TODO: free stuff
+        free(opts->host_ip);
         return -1;
     }
-    char *path = strdup(argv[1]);
+
+    char *path = strdup(argv[DIR_INDEX]);
     printf("Pre-Sanitized Path: %s\n", path);
     sanitize_path(path);
     printf("Sanitized Path: %s\n", path);
     opts->dir_path = path;
+
+    opts->host_port = parse_in_port_t(argv[PORT_INDEX]);
+
+    printf("---------------------------- Server Options ----------------------------\n");
+    printf("Server IP Address: %s\n", opts->host_ip);
+    printf("Server Domain: %d\n", opts->domain);
+    printf("Server Port: %hu\n", opts->host_port);
+    printf("Server Directory: %s\n", opts->dir_path);
+    printf("---------------------------- Server Options ----------------------------\n");
+
     return 0;
 
+}
+
+int set_domain(struct server_options *opts)
+{
+    if(opts->host_ip == NULL)
+    {
+        printf("no host ip\n");
+        return -1;
+    }
+    if(strrchr(opts->host_ip, ':') != NULL)
+    {
+        printf("IPv4\n");
+        opts->domain = AF_INET6;
+        return 0;
+    }
+    else if(strrchr(opts->host_ip, '.') != NULL)
+    {
+        printf("IPv6\n");
+        opts->domain = AF_INET;
+        return 0;
+    }
+    else
+    {
+        printf("neither a ipv4 nor a ipv6 address\n");
+        return -1;
+    }
+}
+
+in_port_t parse_in_port_t(const char *str)
+{
+    char *endptr;
+    uintmax_t parsed_value;
+
+    parsed_value = strtoumax(str, &endptr, 10);
+
+    if (errno != 0)
+    {
+        perror("Error parsing in_port_t");
+        exit(EXIT_FAILURE);
+    }
+
+    // Check if there are any non-numeric characters in the input string
+    if (*endptr != '\0')
+    {
+        printf("Invalid characters in input.\n");
+    }
+
+    // Check if the parsed value is within the valid range for in_port_t
+    if (parsed_value > UINT16_MAX)
+    {
+        printf("in_port_t value out of range.\n");
+    }
+
+    return (in_port_t)parsed_value;
 }
 
 int is_valid_char(const char c) {
@@ -100,8 +232,11 @@ int is_valid_char(const char c) {
 
 void sanitize_path(char *path)
 {
+    wordexp_t p;
+
     //change path so that it's a valid dir path
     size_t len = strlen(path);
+
     for(size_t i = 0; i < len; ++i)
     {
         if(is_valid_char(path[i]) == 0)
@@ -109,6 +244,14 @@ void sanitize_path(char *path)
             path[i] = '-';
         }
     }
+
+    if(wordexp(path, &p, 0) != 0)
+    {
+        printf("bad wordexp\n");
+    }
+    printf("expanded str: %s\n", p.we_wordv[0]);
+    path = strdup(p.we_wordv[0]);
+    wordfree(&p);
 }
 
 int make_dir(char *dir_path)
@@ -127,11 +270,11 @@ int make_dir(char *dir_path)
     return 0;
 }
 
-int creat_socket(void)
+int creat_socket(int domain, int type, int options)
 {
     int sockfd;
 
-    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    sockfd = socket(domain, type, options);
 
     if(sockfd == -1)
     {
@@ -142,23 +285,52 @@ int creat_socket(void)
     return sockfd;
 }
 
-int do_bind(int bindfd)
+int do_bind(int bindfd, struct server_options *opts)
 {
+    struct sockaddr_in ipv4_addr;
+    struct sockaddr_in6 ipv6_addr;
 
-    struct sockaddr_un addr;
+    memset(&ipv4_addr, 0, sizeof(ipv4_addr));
+    memset(&ipv6_addr, 0, sizeof(ipv6_addr));
 
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, DOMAIN_PATH, sizeof(addr.sun_path) - 1);
-    addr.sun_path[sizeof(addr.sun_path) - 1] = '\0';
-     if(bind(bindfd, (struct sockaddr*)&addr, sizeof(addr)) == -1)
-     {
-         perror("bind");
-         return -1;
-     }
+    if(opts->domain == AF_INET)
+    {
+        if(inet_pton(opts->domain, opts->host_ip, &ipv4_addr) != 1)
+        {
+            perror("Invalid IP address");
+            return -1;
+        }
+        ipv4_addr.sin_family = AF_INET;
+        ipv4_addr.sin_port = htons(opts->host_port);
+        if(bind(bindfd, (struct sockaddr *)&ipv4_addr, sizeof(ipv4_addr)) == -1)
+        {
+            perror("Binding failed");
+            return -1;
+        }
+    }
+    else if(opts->domain == AF_INET6)
+    {
+        if(inet_pton(opts->domain, opts->host_ip, &ipv6_addr) != 1)
+        {
+            perror("Invalid IP address");
+            return -1;
+        }
+        ipv6_addr.sin6_family = AF_INET6;
+        ipv6_addr.sin6_port = htons(opts->host_port);
+        if(bind(bindfd, (struct sockaddr *)&ipv6_addr, sizeof(ipv6_addr)) == -1)
+        {
+            perror("Binding failed");
+            return -1;
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Invalid domain: %d\n", opts->domain);
+        return -1;
+    }
 
-     printf("Bound to domain socket: %s\n", DOMAIN_PATH);
-     return 0;
+    printf("Bound to socket: %s:%u\n", opts->host_ip, opts->host_port);
+    return 0;
 
 }
 
@@ -174,19 +346,66 @@ int do_listen(int bindfd, int backlog)
     return 0;
 }
 
-void handle_connection(int acceptfd, struct sockaddr_storage *client_addr, struct server_options *opts)
+//void handle_connection(int acceptfd, struct sockaddr_storage *client_addr, struct server_options *opts)
+//{
+//    struct file_info file;
+//    ssize_t rbytes;
+//
+//    //read file_name_size
+//    while(!exit_flag)
+//    {
+//        memset(&file, 0, sizeof(struct file_info));
+//        rbytes = read(acceptfd, &file.file_name_size, 1);
+//        if(rbytes <= 0)
+//        {
+//            break;
+//        }
+//        printf("-------------------------------------------\n");
+//        printf("File name size: %d\n", file.file_name_size);
+//
+//        //read file_name
+//        file.file_name = malloc(file.file_name_size+1);
+//        if(file.file_name == NULL)
+//        {
+//            printf("error malloc-ing\n");
+//            return;
+//        }
+//        read(acceptfd, file.file_name, file.file_name_size);
+//        file.file_name[file.file_name_size] = '\0';
+//        printf("File name: %s\n", file.file_name);
+//
+//        //read file_size
+//        read(acceptfd, &file.file_size, sizeof(file.file_size));
+//        printf("File size: %lld\n", file.file_size);
+//
+//        //create file
+//        printf("Creating file...\n");
+//        create_file(opts, &file);
+//        //copy file content over to created file
+//        copy_paste(acceptfd, file.filefd, file.file_size);
+//        printf("File contents copied\n");
+//        close(file.filefd);
+//        free(file.file_name);
+//    }
+//    printf("Client disconnected\n");
+//
+//}
+
+void handle_data_in(int client_fd, struct server_options *opts, fd_set *readfds, int *client_sockets, size_t i)
 {
     struct file_info file;
     ssize_t rbytes;
 
-    //read file_name_size
-    while(!exit_flag)
-    {
         memset(&file, 0, sizeof(struct file_info));
-        rbytes = read(acceptfd, &file.file_name_size, 1);
+        rbytes = read(client_fd, &file.file_name_size, 1);
         if(rbytes <= 0)
         {
-            break;
+              // Connection closed or error
+              printf("Client %d disconnected\n", client_fd);
+              close(client_fd);
+              FD_CLR((unsigned int)client_fd, readfds); // Remove the closed socket from the set
+              client_sockets[i] = 0;
+
         }
         printf("-------------------------------------------\n");
         printf("File name size: %d\n", file.file_name_size);
@@ -198,25 +417,39 @@ void handle_connection(int acceptfd, struct sockaddr_storage *client_addr, struc
             printf("error malloc-ing\n");
             return;
         }
-        read(acceptfd, file.file_name, file.file_name_size);
+        rbytes = read(client_fd, file.file_name, file.file_name_size);
+        if(rbytes <= 0)
+        {
+            // Connection closed or error
+            printf("Client %d disconnected\n", client_fd);
+            close(client_fd);
+            FD_CLR((unsigned int)client_fd, readfds); // Remove the closed socket from the set
+            client_sockets[i] = 0;
+        }
         file.file_name[file.file_name_size] = '\0';
         printf("File name: %s\n", file.file_name);
 
         //read file_size
-        read(acceptfd, &file.file_size, sizeof(file.file_size));
+        rbytes = read(client_fd, &file.file_size, sizeof(file.file_size));
+        if(rbytes <= 0)
+        {
+            // Connection closed or error
+            printf("Client %d disconnected\n", client_fd);
+            close(client_fd);
+            FD_CLR((unsigned int)client_fd, readfds); // Remove the closed socket from the set
+            client_sockets[i] = 0;
+
+        }
         printf("File size: %lld\n", file.file_size);
 
         //create file
         printf("Creating file...\n");
         create_file(opts, &file);
         //copy file content over to created file
-        copy_paste(acceptfd, file.filefd, file.file_size);
+        copy_paste(client_fd, file.filefd, file.file_size);
         printf("File contents copied\n");
         close(file.filefd);
         free(file.file_name);
-    }
-    printf("Client disconnected\n");
-
 }
 
 int create_file(struct server_options *opts, struct file_info *file)
@@ -300,6 +533,15 @@ void copy_paste(int src_fd, int dest_fd, int64_t count)
     }
 
     free(buffer);
+}
+
+void socket_close(int fd)
+{
+    if (close(fd) == -1)
+    {
+        perror("Error closing socket");
+        exit(EXIT_FAILURE);
+    }
 }
 
 #pragma GCC diagnostic push
